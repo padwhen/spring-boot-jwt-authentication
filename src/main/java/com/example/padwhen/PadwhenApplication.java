ed04074f4a8457e6d610a51d70d7a2da6d621676
padwhen <123895854+padwhen@users.noreply.github.com>
2024-03-17 21:10:56 +0200

    first commit

---
+++ src/main/java/com/example/padwhen/PadwhenApplication.java
@@ -0,0 +1,13 @@
+package com.example.padwhen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadwhenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadwhenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/padwhen/PadwhenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.padwhen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadwhenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
